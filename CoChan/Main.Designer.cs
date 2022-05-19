b50a7310ff8d14168f2319534d1f12ce655c3bfa
fl-wer <101416707+fl-wer@users.noreply.github.com>
2022-05-19 19:37:44 +0100

    Add files via upload

---
+++ CoChan/Main.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace CoChan
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.bannerLogo = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.bannerLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bannerLogo
+            // 
+            this.bannerLogo.Image = ((System.Drawing.Image)(resources.GetObject("bannerLogo.Image")));
+            this.bannerLogo.Location = new System.Drawing.Point(-1, 0);
+            this.bannerLogo.Name = "bannerLogo";
+            this.bannerLogo.Size = new System.Drawing.Size(401, 160);
+            this.bannerLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bannerLogo.TabIndex = 1;
+            this.bannerLogo.TabStop = false;
+            this.bannerLogo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Main_MouseDown);
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 160);
+            this.Controls.Add(this.bannerLogo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CoChan";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Main_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.bannerLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bannerLogo;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+    }
+}
+
---
+++ CoChan/Main.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoChan
+{
+    public partial class Main : Form
+    {
+        // initializing, ignore this
+        public Main() { InitializeComponent(); }
+
+        // ### moving window with mouse
+        // importing win libraries
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        // function launched when picture box or form is clicked on
+        private void Main_MouseDown(object sender, MouseEventArgs e)
+        {
+            // if left button pressed and held
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, 0xA1, 0x02, 0);
+            }
+        }
+
+        // starting different thread so you could move the window while it's doing its thing
+        private void Main_Load(object sender, EventArgs e) { backgroundWorker.RunWorkerAsync(); }
+
+        // full thread that was mentioned above
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // getting all files from the .exe location
+            string exeLocation = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string[] foundFiles = Directory.GetFiles(exeLocation);
+
+            // looking for exp files until the exp + (matchesCounter+1) + .txt is not there
+            int matchesCounter = 0;
+            while (true)
+            {
+                // going through files array
+                bool foundMatch = false;
+                foreach (string filePath in foundFiles)
+                {
+                    if (Path.GetFileName(filePath) == "exp" + (matchesCounter + 1) + ".txt")
+                    {
+                        matchesCounter += 1;
+                        foundMatch = true;
+                    }
+                }
+
+                // stop loop if found nothing
+                if (!foundMatch) break;
+            }
+
+            // making sure if at least one file found
+            if (matchesCounter > 0)
+            {
+                // load exp file directories into string array
+                string[] exportFilesPath = new string[matchesCounter];
+
+                for (int i = 0; i < exportFilesPath.Length; i++)
+                    exportFilesPath[i] = Path.Combine(exeLocation, "exp" + (i + 1 + ".txt"));
+
+                // connecting all lines from all files
+                string allLines = "";
+                foreach (string expPath in exportFilesPath)
+                    allLines += File.ReadAllText(expPath);
+
+                // splitting into array with lines
+                string[] allLinesSplit = allLines.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+                // converting all lines to just get the channel links
+                for (int i = 0; i < allLinesSplit.Length; i++)
+                {
+                    // checks to make sure line has all we need
+                    if (allLinesSplit[i].Contains("\"channel\": \"") && allLinesSplit[i].Contains(", \"votes\":"))
+                    {
+                        // converting line to just channel link
+                        string channelLink = allLinesSplit[i].Remove(0, allLinesSplit[i].IndexOf("\"channel\": \"") + 12);
+                        channelLink = channelLink.Remove(channelLink.IndexOf(", \"votes\":") - 1);
+                        channelLink = "https://www.youtube.com/channel/" + channelLink;
+
+                        // saving in the array index
+                        allLinesSplit[i] = channelLink;
+                    }
+                }
+
+                // writing all channel links to txt file
+                File.WriteAllLines("once upon a time.txt", allLinesSplit);
+            }
+            else MessageBox.Show("Couldn't find any \"exp\" files..", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            // closing after exporting or after an error
+            Application.Exit();
+        }
+    }
+}
